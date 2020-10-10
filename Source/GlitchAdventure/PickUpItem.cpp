4019a5b19dc277dcb7e3a6f2a942ae0f18e7b233
goatryder <nova9ok@gmail.com>
2020-10-10 09:38:52 +0300

    coins

---
+++ Source/GlitchAdventure/PickUpItem.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PickUpItem.h"
+
+#include "Components/SphereComponent.h"
+
+// Sets default values
+APickUpItem::APickUpItem()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	RootComponent = Mesh;
+
+	CollisionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("Collision Sphere"));
+	CollisionSphere->SetupAttachment(Mesh);
+
+}
+
+// Called when the game starts or when spawned
+void APickUpItem::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void APickUpItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
